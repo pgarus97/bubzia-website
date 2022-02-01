f4d4322b04d46347aa2abe716fcff46e3511541c
Philip Garus <philipgarus97@gmail.com>
2022-02-01 18:40:56 +0100

    added sidebar to podcasts

--- src/components/Podcasts.js
+++ src/components/Podcasts.js
@@ -10,18 +10,41 @@ const view = 'coverart'; // or 'coverart'
 const theme = 'black'; // or 'white'
 
 const Podcasts = () => {
+
+    const selectDropDown = name => {
+        setDropName(name);
+    }
+
+    const [dropName, setDropName] = useState("All Podcasts");
+
+
+    //can add more links if needed
+    const dropOptions = [
+        {
+            id:1,
+            text:"All Podcasts",
+        },{
+            id:2,
+            text:"Youtube",
+        },
+        {
+            id:3,
+            text:"Spotify",
+        }
+    ]
+
     //can add more links if needed
     const links = [
         {
             id:1,
             url: "https://www.youtube.com/watch?v=RUfM7IWu3pQ",
             text:"OverBoost #50 Podcast",
-            format: "YT"
+            format: "Youtube"
         },{
             id:2,
             url:"https://www.youtube.com/watch?v=pfUw8S5z6oY",
             text:"Zack!!! Podcast (German)",
-            format: "YT"
+            format: "Youtube"
         },
         {
             id:3,
@@ -41,12 +64,30 @@ const Podcasts = () => {
     //looping through links to get individual items
     //dynamically add classname for css
     return (
-        <ul className="mainplayers">
+        <div className="marathoncenter">
+            <nav style={{ paddingLeft:"10%", position:"absolute" }}>
+                <ul className={`marathonNav.showMenu"`}>
+                    <li className="sidebarTop">Selection</li>
+                    {dropOptions.map(link => {
+                        return(
+                            <li className={`item ${dropName==link.text ? " active" : ""}`} key={link.id}>
+                                <span
+                                    style={{marginRight:"15px"}}
+                                    onClick={() => selectDropDown(link.text)}
+                                >
+                                    {link.text}
+                                </span>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav><ul className="mainplayers">
             {links.map(link => {
-                    return(
+                if (dropName === "All Podcasts") {
+                    return (
                         <li key={link.id}>
                             <h4 className="cleanfont">{link.text}</h4>
-                            { link.format === "YT" ? <ReactPlayer url={link.url} controls="true"/> : <SpotifyPlayer
+                            {link.format === "Youtube" ? <ReactPlayer url={link.url} controls="true"/> : <SpotifyPlayer
                                 uri={link.url}
                                 size={size}
                                 view={view}
@@ -55,10 +96,25 @@ const Podcasts = () => {
                             <br/>
                         </li>
                     )
+                }
+                if (dropName === link.format) {
+                    return (
+                        <li key={link.id}>
+                            <h4 className="cleanfont">{link.text}</h4>
+                            {link.format === "Youtube" ? <ReactPlayer url={link.url} controls="true"/> : <SpotifyPlayer
+                                uri={link.url}
+                                size={size}
+                                view={view}
+                                theme={theme}
+                            />}
+                            <br/>
+                        </li>
+                    )
+                }
                 //TODO else case default in case of error?
             })}
         </ul>
-
+        </div>
     )
 }
 export default Podcasts
